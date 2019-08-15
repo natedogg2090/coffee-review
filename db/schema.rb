231a750573b06424ddb017a741d1bf7e2225e853
Nate Dietz <nsdietz@gmail.com>
2019-08-14 22:49:49 -0400

    added User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_023035) do
+ActiveRecord::Schema.define(version: 2019_08_15_024716) do
 
   create_table "roasts", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_08_15_023035) do
     t.string "prep"
     t.integer "rating"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "money"
+    t.boolean "admin"
+    t.string "prep_pref"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
