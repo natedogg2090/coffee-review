f9091c003747f28d92cf2cb8aecbde119f8376e5
Nate Dietz <nsdietz@gmail.com>
2019-08-17 13:53:18 -0400

    form to submit new roasts and updated roast/user model association

--- app/controllers/roasts_controller.rb
+++ app/controllers/roasts_controller.rb
@@ -7,4 +7,22 @@ class RoastsController < ApplicationController
   def new
     @roast = Roast.new
   end
+
+  def create
+
+    roast = Roast.new(
+      :name => params[:roast][:name], 
+      :origin => params[:roast][:origin], 
+      :tasting_notes => params[:roast][:tasting_notes], 
+      :prep => params[:roast][:prep], 
+      :price => params[:roast][:price])
+
+    roaster = Roaster.find_or_create_by(:name => params[:roast][:roaster])
+    roast.roaster_id = roaster.id
+
+    roast.save
+
+    redirect_to roasts_path
+  end
+
 end
