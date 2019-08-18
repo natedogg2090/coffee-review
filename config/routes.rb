9e675cd6ea1b2e5dc05824253a8e170d8d86a91c
Nate Dietz <nsdietz@gmail.com>
2019-08-17 23:16:04 -0400

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :roasts, only: [:index, :new, :create, :show, :edit, :update]
   resources :roasters, only: [:index, :show, :edit, :update, :new, :create]
+  resources :users, only: [:new, :create]
 end
