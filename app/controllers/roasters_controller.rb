9e34e77e07e2806907deb4a54d04be823548d24f
Nate Dietz <nsdietz@gmail.com>
2019-08-17 21:57:30 -0400

    able to edit roaster details

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -11,4 +11,11 @@ class RoastersController < ApplicationController
   def edit
     @roaster = Roaster.find_by(:id => params[:id])
   end
+
+  def update
+    roaster = Roaster.find_by(:id => params[:id])
+    roaster.update(:name => params[:roaster][:name], :location => params[:roaster][:location])
+
+    redirect_to roaster_path(roaster)
+  end
 end
