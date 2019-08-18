d27359474552f72943c6ce962b290514be5ab71d
Nate Dietz <nsdietz@gmail.com>
2019-08-18 17:21:19 -0400

    modified user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :money, :prep_pref, :admin, :password)
+    params.require(:user).permit(:name, :password, :money, :admin)
   end
 end
