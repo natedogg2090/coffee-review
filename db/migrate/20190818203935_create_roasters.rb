f3847639e8fef15f652db837c15437c7e2140442
Nate Dietz <nsdietz@gmail.com>
2019-08-18 16:41:00 -0400

    reset db

--- db/migrate/20190818203935_create_roasters.rb
+++ db/migrate/20190818203935_create_roasters.rb
@@ -1,4 +1,4 @@
-class Roasters < ActiveRecord::Migration[5.2]
+class CreateRoasters < ActiveRecord::Migration[5.2]
   def change
     create_table :roasters do |t|
       t.string :name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_203441) do
+ActiveRecord::Schema.define(version: 2019_08_18_203935) do
 
   create_table "roasters", force: :cascade do |t|
     t.string "name"
@@ -18,10 +18,6 @@ ActiveRecord::Schema.define(version: 2019_08_18_203441) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password_digest"
-    t.integer "money"
-    t.boolean "admin", default: false
   end
 
 end
