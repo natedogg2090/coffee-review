c51ee641611939ae4efdc9b4b2d6d3f654d793b7
Nate Dietz <nsdietz@gmail.com>
2019-08-17 23:23:19 -0400

    added user show view and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  def create
+    user = User.new(:name => params[:user][:name], :money => params[:user][:money], :prep_pref => params[:user][:prep_pref])
+    user.save
+
+    redirect_to user_path(user)
+  end
+
+  def show
+    @user = User.find_by(:id => params[:id])
+  end
 end
