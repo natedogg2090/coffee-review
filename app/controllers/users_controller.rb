530c8a30b52694294013c5216bcbffcca6b958f7
Nate Dietz <nsdietz@gmail.com>
2019-08-18 14:11:20 -0400

    added pw on login view and bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :money, :prep_pref, :admin)
+    params.require(:user).permit(:name, :money, :prep_pref, :admin, :password)
   end
 end
