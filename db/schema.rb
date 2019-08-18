e7524d159df6eb5a491c322b3af6f53338a3f01a
Nate Dietz <nsdietz@gmail.com>
2019-08-18 13:04:51 -0400

    added column to user db for pw at login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_002203) do
+ActiveRecord::Schema.define(version: 2019_08_18_170304) do
 
   create_table "roasters", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_08_16_002203) do
     t.string "prep_pref"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
