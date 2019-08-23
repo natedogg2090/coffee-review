a26c2acf77145f741bef6a540d4e46df050e60aa
Nate Dietz <nsdietz@gmail.com>
2019-08-22 20:15:57 -0400

    adding create purchase route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :roasts, only: [:index, :new, :create, :show, :edit, :update]
   resources :roasters, only: [:index, :show, :edit, :update, :new, :create]
   resources :users, only: [:new, :create, :show]
+  resources :purchases, only: [:create]
 
   get 'login' => 'sessions#new'
   post 'sessions' => 'sessions#create'
