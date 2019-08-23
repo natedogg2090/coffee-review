d1484544c0425f738d33c0797dd8515e2ea2dd7c
Nate Dietz <nsdietz@gmail.com>
2019-08-22 21:12:04 -0400

    added buy_now method to model to verify user has enough money to afford roast

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
 
   def create
-    raise params.inspect
+    purchase = Purchase.new(:user_id => params[:purchase][:user_id], :roast_id => params[:purchase][:roast_id])
+    user = User.find_by_id(params[:purchase][:user_id])
   end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,17 @@
 class Purchase < ApplicationRecord
   belongs_to :user
-  belongs_to :roaster
+  belongs_to :roast
+
+  def buy_now
+    roast = Roast.find_by(:id => self.roast_id)
+    user = User.find_by(:id => self.user_id)
+
+    if user.money < roast.price
+      "Sorry. You do not have enough money to purchase this #{roast.name}."
+    else
+      rem_money = user.money - roast.price
+      user.update(:money => rem_money)
+      "Thanks for purchasing the #{roast.name} roast!"
+    end
+  end
 end
