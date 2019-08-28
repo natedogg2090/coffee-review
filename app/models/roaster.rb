1c5b0e882d6fef32c26f7fd1976039d5634aebb3
Nate Dietz <nsdietz@gmail.com>
2019-08-27 22:01:24 -0400

    added all roasts for a roaster to roaster show

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -3,8 +3,8 @@ class Roaster < ApplicationRecord
   has_many :users, through: :roasts
 
   validates :name, presence: {message: "can't be blank"}
-  validates :location, allow_blank: true
+  # validates :location, allow_blank: true
 
-  
+  scope :recent_roasts, -> (time) {where(":roasted_date < ? ", time)}
 
 end
