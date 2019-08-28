fb6a62e92c2df0e90e2974448cea1e7d3ed28e4f
Nate Dietz <nsdietz@gmail.com>
2019-08-27 21:22:21 -0400

    form validations for new roast

--- app/models/roast.rb
+++ app/models/roast.rb
@@ -3,5 +3,12 @@ class Roast < ApplicationRecord
   belongs_to :roaster
 
   validates :name, :origin, :tasting_notes, :preparation_method, :price, presence: {message: "cannot be blank."}
+  validate :date_not_in_future
+
+  def date_not_in_future
+    if roasted_date > Date.today
+      errors.add(:roasted_date, "cannot be in the future.")
+    end
+  end
 
 end
